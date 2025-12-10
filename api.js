7a548335566d1d32995d759be234b0e726719ff1
kwadjo-wusu-ansah <kwadjo.wusu-ansah@amalitech.com>
2025-12-10 16:18:31 +0000

    Add API class for fetching data

---
+++ api.js
@@ -0,0 +1,53 @@
+export class APIClient {
+  constructor(baseURL) {
+    this._baseURL = baseURL;
+  }
+
+  async fetchUsers() {
+    try {
+      const response = await fetch(`${this._baseURL}/users`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Failed to fetch users:", error);
+      throw error;
+    }
+  }
+
+  async fetchTodos() {
+    try {
+      const response = await fetch(`${this._baseURL}/todos`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Failed to fetch todos:", error);
+      throw error;
+    }
+  }
+
+  async fetchUsersTodos(userId) {
+    try {
+      const response = await this.fetchTodos();
+      const data = await response;
+      return data.filter((todo) => todo.userId === userId);
+    } catch (error) {
+      console.error("Failed to fetch user's todos:", error);
+      throw error;
+    }
+  }
+
+  async loadData() {
+    try {
+      const users = await this.fetchUsers();
+      const todos = users.map(async (user) => {
+        const userTodos = await this.fetchUsersTodos(user.id);
+        return userTodos;
+      });
+      const allUserTodos = await Promise.all(todos);
+      return [users, allUserTodos];
+    } catch (error) {
+      console.error("Failed to load data:", error);
+      throw error;
+    }
+  }
+}
